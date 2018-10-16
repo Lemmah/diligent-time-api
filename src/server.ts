a74b8d1633b71409ad784262f5b8859718d7a230
Lemmah <lemayiannakolah@gmail.com>
2018-10-16 19:22:04 +0300

    fix(src): fix tslint warnings
    - add new line to the end of main.routes file.
    - disable tslint for the necessary console.log call.
    
    [Fixes #linting]

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,6 @@ import app from "./app";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  /* tslint:disable-next-line */
   console.log(`Express server is listening on port ${PORT}`);
 });
