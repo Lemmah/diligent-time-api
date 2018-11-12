093876c5323a11bdc17fb927b90b7d4233b1041f
Lemmah <lemayiannakolah@gmail.com>
2018-11-12 12:11:30 +0300

    refactor(todo.controller): handle errors with global handler
    - use async/await for promises.
    - introduce the next function to pass the errors to the global error handler.
    - create the global error handler in the app.
    
    [Finishes #161863657]

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as mongoose from "mongoose";
 import * as morgan from "morgan";
+
+import Error from "./interfaces/error";
 import mainRoutes from "./routes/main.routes";
 
 const mongoUrl: string = process.env.MONGO_URL || "mongodb://localhost/diligentTime";
@@ -12,5 +14,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use("/", mainRoutes);
+app.use((error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  /* tslint:disable-next-line */
+  console.error(error);
+  res.status(error.statusCode || 500).json({ error: error.message });
+});
 
 export default app;
